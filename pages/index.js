fc9ea6495a3aa2f4fe1b02101033cb9cdc154c09
Som3a Elsayed Mohamed <som3a9w9w2000@gmail.com>
2022-02-04 18:48:21 +0200

    really

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import randomParagraphs from '../randomParagraph.json'
 const generatingRandomParagraph = () => {
   const { paragraphs } = randomParagraphs
